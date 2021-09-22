9bb24f50116ea478976a399109180ae601ac8cb2
ganeshdharma.n <ganeshdharma.n@utthunga.com>
2021-09-22 17:00:59 +0530

    tested

--- calculator.py
+++ calculator.py
@@ -4,6 +4,9 @@
 # Program make a simple calculator that can add, subtract, multiply and divide using functions
 
 # define functions
+x= int(7)
+y = int(8)
+
 def add(x, y):
     """This function adds two numbers"""
 
@@ -27,29 +30,32 @@ def divide(x, y):
 
     return x / y
 
-
-# take input from the user
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-choice = input("Enter choice(1/2/3/4):")
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
-
-elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
-
-elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
-
-elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Invalid input")
+print(add(x,y))
+print(subtract(x,y))
+print(multiply(x,y))
+print(divide(x,y))
+# # # take input from the user
+# print("Select operation.")
+# print("1.Add", 1)
+# print("2.Subtract")
+# print("3.Multiply")
+# print("4.Divide")
+# #
+# # choice = input("Enter choice(1/2/3/4):")
+# #
+# #  num1 = int(input("Enter first number: "))
+# #  num2 = int(input("Enter second number: "))
+#
+# if choice == '1':
+#     print(num1, "+", num2, "=", add(num1, num2))
+#
+# elif choice == '2':
+#     print(num1, "-", num2, "=", subtract(num1, num2))
+#
+# elif choice == '3':
+#     print(num1, "*", num2, "=", multiply(num1, num2))
+#
+# elif choice == '4':
+#     print(num1, "/", num2, "=", divide(num1, num2))
+# else:
+#     print("Invalid input")
